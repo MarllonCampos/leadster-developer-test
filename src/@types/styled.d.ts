57d120566a98522e2b1c159855bec8d179b5c8a6
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-06-23 15:52:01 -0300

    feat: create a type folder and styled file to help intellisense

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,13 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      white: string;
+      'blue-50': string;
+      'blue-100': string;
+      'blue-200': string;
+      text: string;
+    };
+  }
+}
