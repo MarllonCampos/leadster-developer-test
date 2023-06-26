cc47f7dc99d7ce06dcffa82026640936a529143e
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-06-26 02:23:44 -0300

    feat: create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Container, SubHeaderContainer } from './styles';
+import { Container, Copyright, FooterNav, SubHeaderContainer } from './styles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
@@ -10,6 +11,21 @@ const Footer: React.FC = () => {
         <Image src={'/logo.png'} width={212} height={42} alt="Leadster Logo" />
         <p>Transformando visitantes em clientes.</p>
       </SubHeaderContainer>
+      <FooterNav></FooterNav>
+      <Copyright className="copyright">
+        <div className="copyright__content">
+          <p className="copyright__text">
+            Copyright &copy; 2015 - 2022 Todos os direitos reservados &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link href="/" className="copyright__text-accent">
+              Leadster
+            </Link>
+          </p>
+
+          <p className="copyright__address">
+            Rua José Loureiro, 464 - Centro - Curitiba PR - CEP 80010-000 | Termos de uso
+          </p>
+        </div>
+      </Copyright>
     </Container>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,15 +1,59 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  padding: 40px 0;
+  padding-top: 40px;
 `;
 
 export const SubHeaderContainer = styled.header`
   text-align: center;
   font-size: 12px;
   color: ${({ theme }) => theme.colors.text};
+`;
+
+const copyrightMediaQuery = css`
+  @media screen and (max-width: 865px) {
+    .copyright {
+      &__content {
+        align-items: center;
+        flex-direction: column;
+      }
+    }
+  }
+`;
+
+export const Copyright = styled.div.attrs({ className: 'copyright' })`
+  width: 100%;
+  padding: 40px 12px;
+  font-size: 12px;
+
+  box-shadow: 0px -1px 1px ${({ theme }) => theme.colors.shadow};
+  .copyright {
+    &__content {
+      max-width: 1140px;
+      margin: 0 auto;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+    ${({ theme }) => css`
+      &__text {
+        &-accent {
+          text-decoration: none;
+          font-weight: bold;
+        }
+      }
+    `}
+  }
+
+  ${copyrightMediaQuery}
+`;
+
+export const FooterNav = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
 `;
