f4bfcacb0d9b19b85e35572578310a476b61dc94
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-06-28 15:51:48 -0300

    feat: add lead generation component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import { Button } from '@/components/Button';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import LeadGeneration from '@/components/LeadGeneration';
 import Presentation from '@/components/Presentation';
 export default function Home() {
   return (
     <>
       <Header />
       <Presentation />
-
+      <LeadGeneration />
       <Footer />
     </>
   );
