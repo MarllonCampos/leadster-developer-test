879d67cc23815d2bdb74fbd6fec68ee996a1de6e
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-06-28 15:50:37 -0300

    feat: create LeadGeneration component, but it need some changes on images

---
+++ src/components/LeadGeneration/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {
+  ButtonsContainer,
+  Container,
+  Content,
+  CreditCardContainer,
+  Rating,
+  RatingSatisfactionContainer,
+  TextContainer,
+} from './styles';
+import Image from 'next/image';
+import { Button } from '../Button';
+
+const LeadGeneration: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <Image
+          src={'/comparativo_img_CTA.png'}
+          width={440}
+          height={440}
+          alt="Comparativo w/o Leadster"
+          className="comparative-image"
+        />
+        <TextContainer>
+          <p className="text-container__heading">Pronto para triplicar sua</p>
+          <strong className="text-container__subtitle">Geração de Leads?</strong>
+          <span className="text-container__text">
+            Criação e ativação em <strong>4 minutos</strong>
+          </span>
+          <hr className="text-container__divider" />
+          <ButtonsContainer>
+            <Button selected className="button-container__view-demo">
+              VER DEMONSTRAÇÃO
+            </Button>
+            <Image src={'/selo_RD.png'} width={180} height={70} alt="Selo do RD" />
+          </ButtonsContainer>
+          <Rating>
+            <CreditCardContainer>
+              <Image src={'/no-card-dark.webp'} width={16} height={16} alt="Sem necessidade de cartão" />
+              <p className="rating__text">
+                <strong>Não</strong> é necessário Cartão de Crédito
+              </p>
+            </CreditCardContainer>
+            <RatingSatisfactionContainer>
+              <Image src={'/rating.webp'} width={92} height={16} alt="Média de sastisfação" />
+              &nbsp;
+              <span className="rating__average-satisfaction">
+                <strong>4,9</strong>/5 média de satisfação
+              </span>
+            </RatingSatisfactionContainer>
+          </Rating>
+        </TextContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default LeadGeneration;
---
+++ src/components/LeadGeneration/styles.ts
@@ -0,0 +1,146 @@
+import styled, { css } from 'styled-components';
+
+const DESKTOP_SIZE = '1082px';
+const MOBILE_SIZE = '478px';
+
+export const Container = styled.div`
+  padding: 40px 0;
+
+  background-color: ${({ theme }) => theme.colors['blue-50']};
+`;
+
+const comparativeImageMediaQuery = css`
+  @media screen and (max-width: 456px) {
+    max-width: 340px;
+    max-height: 340px;
+  }
+`;
+
+export const Content = styled.div`
+  margin: 0 auto;
+
+  max-width: 1180px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+
+  .comparative-image {
+    flex-basis: 320px;
+    ${comparativeImageMediaQuery}
+  }
+`;
+
+const textContainerMediaQuery = css`
+  @media screen and (max-width: ${DESKTOP_SIZE}) {
+    text-align: center;
+  }
+`;
+
+export const TextContainer = styled.div.attrs({ className: 'text-container' })`
+  display: flex;
+  flex-direction: column;
+  padding: 0 12px;
+
+  .text-container {
+    &__heading {
+      color: ${({ theme }) => theme.colors.text};
+      font-size: 34px;
+      font-weight: 500;
+    }
+
+    &__subtitle {
+      color: ${({ theme }) => theme.colors.text};
+      font-size: 36px;
+    }
+
+    &__text {
+      font-weight: 500;
+      color: ${({ theme }) => theme.colors['text-50']};
+    }
+
+    &__divider {
+      margin: 24px 0;
+      border: 0;
+      border-top: 2px solid ${({ theme }) => theme.colors.shadow};
+    }
+  }
+
+  ${textContainerMediaQuery}
+`;
+
+const buttonsContainerMediaQuery = css`
+  @media screen and (max-width: ${DESKTOP_SIZE}) {
+    justify-content: center;
+  }
+`;
+
+export const viewDemoButtonMediaQuery = css`
+  @media screen and (max-width: ${MOBILE_SIZE}) {
+    font-size: 12px;
+  }
+`;
+
+export const ButtonsContainer = styled.div.attrs({ className: 'button-container' })`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  margin-bottom: 20px;
+
+  .button-container {
+    &__view-demo {
+      padding: 14px 34px;
+      ${viewDemoButtonMediaQuery}
+    }
+  }
+  ${buttonsContainerMediaQuery}
+`;
+
+const ratingMediaQuery = css`
+  @media screen and (max-width: 642px) {
+    flex-direction: column;
+  }
+`;
+
+export const Rating = styled.div.attrs({ className: 'rating' })`
+  display: flex;
+  align-items: center;
+
+  .rating {
+    &__text {
+      font-weight: 500;
+      margin-left: 4px;
+      color: ${({ theme }) => theme.colors['text-50']};
+    }
+
+    &__average-satisfaction {
+      font-weight: 500;
+      color: ${({ theme }) => theme.colors['text-50']};
+    }
+  }
+
+  ${ratingMediaQuery}
+`;
+
+const creditCardContainerMediaQuery = css`
+  @media screen and (min-width: 643px) {
+    &:after {
+      content: '|';
+      display: block;
+      margin: 0px 5px;
+    }
+  }
+`;
+
+export const CreditCardContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  ${creditCardContainerMediaQuery}
+`;
+export const RatingSatisfactionContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
