cd993a24f79bf8916142445f495c2a9ce53c376a
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-06-29 16:31:49 -0300

    fix: update imports on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { Button } from '@/components/Button';
-import Footer from '@/components/Footer';
+import Footer from '@/components/Sections/Footer';
 import Header from '@/components/Header';
-import LeadGeneration from '@/components/LeadGeneration';
-import Presentation from '@/components/Presentation';
+import LeadGeneration from '@/components/Sections/LeadGeneration';
+import Presentation from '@/components/Sections/Presentation';
+import VideosList from '@/components/Sections//VideosList';
 export default function Home() {
   return (
     <>
       <Header />
       <Presentation />
+      <VideosList />
       <LeadGeneration />
       <Footer />
     </>
