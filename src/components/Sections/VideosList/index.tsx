89bf6f2ed67d4a46a76bc4f6d57a76d25a79f564
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-07-01 23:26:16 -0300

    feat: initiate the logic to get the values from list

--- src/components/Sections/VideosList/index.tsx
+++ src/components/Sections/VideosList/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { Container, FiltersContainer } from './styles';
 import ListFilter from '@/components/ListFilter';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 const VideosList: React.FC = () => {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const router = useRouter();
+  const orderParam = searchParams.get('order');
+  const filterParam = searchParams.get('filter');
+  const [filter, setFilter] = useState(filterParam || '');
+  const [order, setOrder] = useState(orderParam || '');
+
+  const x = useCallback(() => {
+    console.log(order, filter);
+  }, [order, filter]);
+
+  const filterChange = (value: string) => {
+    console.log(value);
+  };
+
+  const orderChange = (value: string) => {};
+
   return (
     <Container>
       <FiltersContainer>
-        <ListFilter />
+        <ListFilter handleFilterChange={filterChange} handleOrderChange={orderChange} filterSelected={filterParam} />
       </FiltersContainer>
     </Container>
   );
--- src/components/Sections/VideosList/styles.ts
+++ src/components/Sections/VideosList/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  padding: 50px 0;
+  padding: 50px 10px;
   max-width: 1180px;
   margin: 0 auto;
 `;
