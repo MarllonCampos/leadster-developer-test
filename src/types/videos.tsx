ac036c686fd9e9b0b1845046e4e78cba587c39ac
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-07-03 23:47:46 -0300

    feat: create a file that will hold the video types

---
+++ src/types/videos.tsx
@@ -0,0 +1,16 @@
+type DownloadChipsType = 'xls' | 'doc' | 'ppt' | 'zip';
+
+type DownloadChips = {
+  name: string;
+  type: DownloadChipsType;
+};
+
+type VideoInfoType = 'agencias' | 'chatbot' | 'marketing-digital' | 'geracao-de-leads' | 'midia-paga';
+
+export type VideoInfo = {
+  title: string;
+  description: string;
+  downloads: Array<DownloadChips>;
+  created_at: Date;
+  type: VideoInfoType;
+};
