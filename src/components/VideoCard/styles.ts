86c830fc403fa207b37b2ca8411294c084f37a59
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-07-04 03:58:57 -0300

    refactor: changing css to improve UI

--- src/components/VideoCard/styles.ts
+++ src/components/VideoCard/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
   border-radius: 16px;
   cursor: pointer;
   transition: box-shadow 0.2s linear;
@@ -11,11 +14,14 @@ export const Container = styled.div`
   }
 
   max-width: 370px;
+  height: 292px;
 `;
 
 export const Bottom = styled.div.attrs({ className: 'bottom' })`
   padding: 16px 24px;
-
+  display: flex;
+  align-items: center;
+  flex: 1;
   .bottom {
     &__title {
       font-weight: bold;
