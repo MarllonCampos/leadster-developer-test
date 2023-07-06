1532f5d15d2ce8ed6f95e4188c6021a8602456ba
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-07-06 17:35:05 -0300

    feat: finished videoModal component that holds info from videos

--- src/components/VideoModal/index.tsx
+++ src/components/VideoModal/index.tsx
@@ -1,23 +1,74 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, MouseEvent } from 'react';
 import { createPortal } from 'react-dom';
 
-import { Container, Content, CloseContainer } from './styles';
+import {
+  Container,
+  Content,
+  CloseContainer,
+  Video,
+  DescriptionContainer,
+  ModalTitles,
+  DownloadsContainer,
+} from './styles';
 import { VideoInfo } from '@/types/videos';
+import DownloadChips from '../DownloadChips';
 type Props = {
   video: VideoInfo;
   handleCloseModal: () => void;
 };
 const VideoModal: React.FC<Props> = (props) => {
+  useEffect(() => {
+    window.document.body.classList.add('hidden');
+    return () => {
+      window.document.body.classList.remove('hidden');
+    };
+  }, []);
+
+  function handleContentContainerClick(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
   return createPortal(
-    <Container>
-      <Content>
-        <CloseContainer></CloseContainer>
+    <Container onClick={props.handleCloseModal}>
+      <Content onClick={handleContentContainerClick}>
+        <CloseContainer>
+          <svg
+            width="24px"
+            height="24px"
+            viewBox="0 0 64 64"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="#578696"
+            role="button"
+            onClick={props.handleCloseModal}
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <line x1="16" y1="16" x2="48" y2="48"></line>
+              <line x1="48" y1="16" x2="16" y2="48"></line>
+            </g>
+          </svg>
+        </CloseContainer>
         <h3 className="content__heading">
-          <strong className="content__default-title">Webinar:</strong>
+          <strong className="content__default-title">Webinar: </strong>
           {props.video.title}
         </h3>
+        <Video />
+        <DescriptionContainer>
+          <ModalTitles className="description-container__title">Descrição</ModalTitles>
+          <hr className="description-container__divider" />
+          <p className="description-container">{props.video.description}</p>
+          <ModalTitles className="description-container__downloads-title">Downloads</ModalTitles>
+          <hr className="description-container__divider" />
+          <DownloadsContainer>
+            {props.video.downloads.map(({ name, type }, index) => (
+              <DownloadChips key={`${index}-${name}`} title={name} type={type} />
+            ))}
+          </DownloadsContainer>
+        </DescriptionContainer>
       </Content>
     </Container>,
     document.body
--- src/components/VideoModal/styles.ts
+++ src/components/VideoModal/styles.ts
@@ -1,23 +1,99 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: absolute;
+  position: fixed;
   inset: 0;
   background-color: rgb(118, 137, 152, 0.5);
   backdrop-filter: blur(2px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Content = styled.div.attrs({ className: 'content' })`
   background-color: ${({ theme }) => theme.colors.white};
+  padding: 24px 0;
+  border-radius: 12px;
+  width: clamp(300px, 90%, 600px);
+  position: relative;
+  overflow: auto;
+  max-height: 90dvh;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  &::-webkit-scrollbar-track-piece {
+    background-color: ${({ theme }) => theme.colors.shadow};
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 2px ${({ theme }) => theme.colors.shadow};
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors['blue-200']};
+
+    border-radius: 999px;
+  }
 
   .content {
     &__heading {
-      ${({ theme }) => theme.colors.text};
+      color: ${({ theme }) => theme.colors.text};
+      text-align: center;
+      max-width: 360px;
+      margin: 0 auto;
     }
 
     &__default-title {
-      ${({ theme }) => theme.colors['blue-200']};
+      color: ${({ theme }) => theme.colors['blue-200']};
     }
   }
 `;
-export const CloseContainer = styled.div``;
+export const CloseContainer = styled.div`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  text-align: right;
+  &:hover svg {
+    background-color: ${({ theme }) => theme.colors.shadow};
+    border-radius: 6px;
+    cursor: pointer;
+  }
+`;
+
+export const Video = styled.iframe.attrs({ src: 'https://www.youtube.com/embed/o-YBDTqX_ZU' })`
+  width: 100%;
+  height: auto;
+  margin: 30px 0;
+  min-height: 300px;
+  border: 0;
+`;
+
+export const DescriptionContainer = styled.div.attrs({ className: 'description-container' })`
+  padding: 0 24px;
+  .description-container {
+    &__title {
+    }
+    &__divider {
+      border: 0;
+      margin: 4px 0 12px 0;
+      border-top: 2px solid ${({ theme }) => theme.colors.shadow};
+    }
+  }
+`;
+
+export const ModalTitles = styled.p`
+  color: ${({ theme }) => theme.colors['text-50']};
+  font-weight: 700;
+  margin-top: 12px;
+`;
+
+export const DownloadsContainer = styled.div`
+  margin-top: 12px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
